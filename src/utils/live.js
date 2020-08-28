e2eb592d3570eb35e44fc60b72399c868788d721
Lauren Davidson <ldavidson@g.harvard.edu>
2020-08-28 11:09:19 -0400

    Add temp err callbacks to live.js

--- src/utils/live.js
+++ src/utils/live.js
@@ -3,13 +3,31 @@ import firebase from 'firebase'
 export default function liveUpdate(action) {
   switch (action.type) {
     case 'RESET':
-      firebase.database().ref(`${action.uid}/live`).set(true)
+      firebase.database().ref(`${action.uid}/live`)
+        .set(true, err => {
+          if (err) {
+            console.log(err)
+            return
+          }
+        })
       return
     case 'NUMCHARS':
-      firebase.database().ref(`${action.uid}/live`).set({numChars: action.num})
+      firebase.database().ref(`${action.uid}/live`)
+        .set({numChars: action.num}, err => {
+          if (err) {
+            console.log(err)
+            return
+          }
+        })
       return
     case 'NAMES':
-      firebase.database().ref(`${action.uid}/live/names/${action.i}`).set(action.value)
+      firebase.database().ref(`${action.uid}/live/names/${action.i}`)
+      .set(action.value, err => {
+        if (err) {
+          console.log(err)
+          return
+        }
+      })
       return
     case 'ANSWERER':
       firebase.database().ref(`${action.uid}/live/answerer`)
@@ -21,13 +39,31 @@ export default function liveUpdate(action) {
         })
       return
     case 'BUILDER':
-      firebase.database().ref(`${action.uid}/live/builders/${action.i}/${action.property}`).set(action.value)
+      firebase.database().ref(`${action.uid}/live/builders/${action.i}/${action.property}`)
+        .set(action.value, err => {
+          if (err) {
+            console.log(err)
+            return
+          }
+        })
       return
     case 'CONNECTIVE':
-      firebase.database().ref(`${action.uid}/live/connective`).set(action.connective)
+      firebase.database().ref(`${action.uid}/live/connective`)
+        .set(action.connective, err => {
+          if (err) {
+            console.log(err)
+            return
+          }
+        })
       return
     case 'ROLES':
-      firebase.database().ref(`${action.uid}/live/roles/${action.name}`).set(action.role)
+      firebase.database().ref(`${action.uid}/live/roles/${action.name}`)
+        .set(action.role, err => {
+          if (err) {
+            console.log(err)
+            return
+          }
+        })
       return
     default:
       console.log(action.type)
