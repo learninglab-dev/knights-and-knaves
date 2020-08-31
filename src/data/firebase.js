b8df0a6da540a33f014b4e95ff81308a3f545d58
Lauren Davidson <ldavidson@g.harvard.edu>
2020-08-31 15:57:02 -0400

    Add data validation for Ask and CharacterBuilder

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -14,30 +14,81 @@ export default function gameDataReducer(data, action) {
         startTime: '',
       }
     case 'CREATE':
-      firebase.database().ref(`/`).update({[action.uid]: true})
+      firebase.database().ref(`/`)
+        .update({[action.uid]: true}, err => {
+          if (err) {
+            console.log(err)
+            alert('we had an issue connecting to the database. sorry about that! please try again.')
+            return data
+          }
+        })
       return {...data, uid: action.uid}
     case 'JOIN':
       return {...data, uid: action.uid, solution: action.solution, turns: action.turns}
     case 'GENERATESOLUTION':
+      for (const num in action.names) {
+        if (!action.names[num]) {
+          alert('looks like you forgot to name one of your characters')
+          return data
+        }
+      }
+      const namesSet = new Set(Object.values(action.names))
+      if (namesSet.size !== Object.values(action.names).length) {
+        alert('all your characters need unique names or the oracle gets confused')
+        return data
+      }
       const roles = chooseRoles(action.names)
-      firebase.database().ref(`/${data.uid}`).update({solution: roles})
+      firebase.database().ref(`/${data.uid}`)
+        .update({solution: roles}, err => {
+          if (err) {
+            console.log(err)
+            alert('we had an issue connecting to the database. sorry about that! please try again.')
+            return data
+          }
+        })
       return {...data, solution: roles}
     case 'SETSOLUTION':
       return {...data, solution: action.solution}
     case 'TAKETURN':
+      if (action.turn === 'invalid') {
+        alert('your question is not valid. check your inputs and try again.')
+        return data
+      }
       if (action.turnType === 'question') {
         if (!action.answerer) {
           alert('looks like you forgot to choose a character to ask')
           return data
         }
         const result = oracle(data.solution, action.answerer, action.turn)
-        firebase.database().ref(`/${data.uid}/turns`).push().set({answerer: action.answerer, question: action.copy, response: result})
+        firebase.database().ref(`/${data.uid}/turns`)
+          .push()
+          .set({answerer: action.answerer, question: action.copy, response: result}, err => {
+            if (err) {
+              console.log(err)
+              alert('we had an issue connecting to the database. sorry about that! please try again.')
+              return data
+            }
+          })
         return data
       }
       const result = checkSolution(action.turn, data.solution)
-      firebase.database().ref(`/${data.uid}/turns`).push().set({solution: action.turn, correct: result})
+      firebase.database().ref(`/${data.uid}/turns`)
+        .push()
+        .set({solution: action.turn, correct: result}, err => {
+          if (err) {
+            console.log(err)
+            alert('we had an issue connecting to the database. sorry about that! please try again.')
+            return data
+          }
+        })
       if (result) {
-        firebase.database().ref(`/${data.uid}/solved`).set(true)
+        firebase.database().ref(`/${data.uid}/solved`).set(true, err => {
+          if (err) {
+            console.log(err)
+            alert('we had an issue connecting to the database. sorry about that! please try again.')
+            return data
+          }
+        })
       }
       return data
     case 'GETTURNS':
@@ -46,5 +97,3 @@ export default function gameDataReducer(data, action) {
       alert('error updating game data')
   }
 }
-
-// TODO: add some error handling
--- src/views/Ask.js
+++ src/views/Ask.js
@@ -105,8 +105,11 @@ export default function Ask() {
   }, [uid, setNegation])
 
   const oracleSpeak = () => {
+    const validatedMb1 = validateQ(mb1)
+    if (!validatedMb1) {
+      return 'invalid'
+    }
     const p = [mb1.predicate, mb1.names? mb1.names : mb1.number ? [mb1.quantifier, mb1.number] : [mb1.quantifier]]
-    const q = [mb2.predicate, mb2.names? mb2.names : mb2.number ? [mb2.quantifier, mb2.number] : [mb2.quantifier]]
     const negations = nots[1] ? nots[2] ? 'BOTH' : 'ONE' : nots[2] ? 'TWO' : 'NEITHER'
     if (!connective) {
       if (negations === 'ONE') {
@@ -117,6 +120,11 @@ export default function Ask() {
       }
       return p
     }
+    const validatedMb2 = validateQ(mb2)
+    if (!validatedMb2) {
+      return 'invalid'
+    }
+    const q = [mb2.predicate, mb2.names? mb2.names : mb2.number ? [mb2.quantifier, mb2.number] : [mb2.quantifier]]
     switch (negations) {
     case 'NEITHER':
       return {
@@ -239,4 +247,24 @@ export default function Ask() {
       </button>
     </>
   )
+}
+
+const validateQ = (builder) => {
+  if (!builder.predicate) {
+    return false
+  }
+  if (!builder.names && !builder.quantifier) {
+    return false
+  }
+  if (builder.quantifier === 'same' || builder.quantifier === 'different') {
+    if (builder.num <= 1 && builder.names.length <= 1) {
+        return false
+    }
+  }
+  if (builder.quantifier === 'most' || builder.quantifier === 'least' || builder.quantifier === 'more' || builder.quantifier === 'less') {
+    if (!builder.num) {
+      return false
+    }
+  }
+  return true
 }
