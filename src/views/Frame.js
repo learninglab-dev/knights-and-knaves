bcfe4dc31fd23196a017be82563a3d01c5f5ab07
philsometimes <philsometimes@gmail.com>
2020-09-15 17:26:28 -0400

    oracle now only shows when gameData.solution exists

--- src/views/Frame.js
+++ src/views/Frame.js
@@ -1,10 +1,13 @@
 import React, { useState, useContext, useEffect } from 'react'
 import {Box, Heading} from 'rebass'
+import { Data } from '../data/GameData'
 import Bug from './Bug'
 import Credits from './Credits'
 import Oracle from './Oracle'
 
 export default function Frame({children}) {
+  const gameData = useContext(Data)
+
   return (
     <Box
       sx={{
@@ -16,7 +19,9 @@ export default function Frame({children}) {
       }}
     >
       <Box sx={{gridColumn:'2/span 1', gridRow:'2/span 1'}}>{children}</Box>
-      <Box sx={{gridColumn:'1/span 3', gridRow:'1/span 1'}}><Oracle /></Box>
+      {gameData.solution &&
+        <Box sx={{gridColumn:'1/span 3', gridRow:'1/span 1'}}><Oracle /></Box>
+      }
       <Box sx={{gridColumn:'1/span 1', gridRow:'3/span 1', placeSelf:'center end'}}><Credits/></Box>
       <Box sx={{gridColumn:'3/span 1', gridRow:'3/span 1', placeSelf:'center start'}}><Bug/></Box>
     </Box>
